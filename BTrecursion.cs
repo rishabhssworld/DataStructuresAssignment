108d37290e1259684c346ad0aecbe0afac0c14de
Rishabh Bhutani <90327492+rishabhssworld@users.noreply.github.com>
2023-03-22 15:47:20 -0400

    Add files via upload

---
+++ BTrecursion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            Console.WriteLine("Pre-order traversal:");
+            tree.Preorder(0);
+            Console.WriteLine();
+            Console.WriteLine("In-order traversal:");
+            tree.Inorder(0);
+            Console.WriteLine();
+            Console.WriteLine("Post-order traversal:");
+            tree.Postorder(0);
+            Console.WriteLine();
+        }
+    }
+    class Tree
+    {
+        /// <summary>
+        /// a function for printing the Preorder
+        /// </summary>
+        /// <param name="n"></param>
+        public void Preorder(int n)
+        {
+            if (n > 9)
+            {
+                return;
+            }
+
+            Console.Write(n + " "); //root
+            Preorder(2 * n + 1); //left node
+            Preorder(2 * n + 2); //right node
+        }
+        /// <summary>
+        /// a function for printing the Inorder
+        /// </summary>
+        /// <param name="n"></param>
+        public void Inorder(int n)
+        {
+            if (n > 9)
+            {
+                return;
+            }
+
+            Inorder(2 * n + 1); //left subtree
+            Console.Write(n + " "); //root
+            Inorder(2 * n + 2); //right subtree
+        }
+        /// <summary>
+        /// a function for printing the Postorder
+        /// </summary>
+        /// <param name="n"></param>
+        public void Postorder(int n)
+        {
+            if (n > 9)
+            {
+                return;
+            }
+            Postorder(2 * n + 1);//left subtree
+            Postorder(2 * n + 2);//right subtree
+            Console.Write(n + " ");//root
+        }
+    }
+}
---
+++ BinaryTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TreeNode
+{
+    public int val; // The value of the node
+    public TreeNode left; // The left child of the node
+    public TreeNode right; // The right child of the node
+    public TreeNode(int x) 
+    {
+        val = x; // Constructor for initializing the node value
+    }
+}
+
+public class BinaryTree
+{
+    public TreeNode root; // The root node of the binary tree
+
+    // Function to print the binary tree in BFS order
+    public void PrintBFS(TreeNode root, int height)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        int level = 0; // The current level of the tree
+        int nodesAtLevel = 1; // The number of nodes at the current level
+        int nodesAtNextLevel = 0; // The number of nodes at the next level
+        int nodeWidth = (int)Math.Pow(2, height - level) - 1; // The width of each node in the current level
+
+        while (queue.Count > 0)
+        {
+            if (level > height)
+            {
+                break;
+            }
+
+            // Print spaces to center the node
+            Console.Write(new string(' ', nodeWidth / 2));
+
+            // Print the nodes at the current level
+            for (int i = 0; i < nodesAtLevel; i++)
+            {
+                TreeNode node = queue.Dequeue();
+
+                // If the node is not null, print its value and enqueue its children
+                if (node != null)
+                {
+                    Console.Write("{0}", node.val);
+                    queue.Enqueue(node.left);
+                    queue.Enqueue(node.right);
+                    nodesAtNextLevel += 2;
+                }
+                // If the node is null, print 'N' and enqueue two null nodes for its children
+                else
+                {
+                    Console.Write("N");
+                    queue.Enqueue(null);
+                    queue.Enqueue(null);
+                    nodesAtNextLevel += 2;
+                }
+
+                // Print spaces to center the next node
+                Console.Write(new string(' ', nodeWidth));
+            }
+
+            Console.WriteLine();
+
+            // Update the variables for the next level
+            nodesAtLevel = nodesAtNextLevel;
+            nodesAtNextLevel = 0;
+            level++;
+
+            // Update the width of each node for the next level
+            nodeWidth = (int)Math.Pow(2, height - level) - 1;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        BinaryTree tree = new BinaryTree();
+
+        // Create a binary tree
+        tree.root = new TreeNode(0);
+        tree.root.left = new TreeNode(1);
+        tree.root.right = new TreeNode(2);
+        tree.root.left.left = new TreeNode(3);
+        tree.root.left.right = new TreeNode(4);
+        tree.root.right.left = new TreeNode(5);
+        tree.root.right.right = new TreeNode(6);
+        tree.root.left.left.left = new TreeNode(7);
+        tree.root.left.left.right = new TreeNode(8);
+        tree.root.left.right.left = new TreeNode(9);
+        tree.root.right.right.left = new TreeNode(10);
+
+        // Print the binary tree in BFS order
+        tree.PrintBFS(tree.root, 4);
+    }
+}
+
